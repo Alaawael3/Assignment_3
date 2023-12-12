23c4f6b35a314e926621475b9e61e652cde16da1
YoussefEhab <114459225+Youkabob@users.noreply.github.com>
2023-12-12 18:51:19 +0200

    Update main.cpp

--- Youka Task2/main.cpp
+++ Youka Task2/main.cpp
@@ -8,17 +8,17 @@ using namespace std;
 int main() {
     int choice;
     Player *player[2];
-    player[0] = new Player(1, "x");
+    player[0] = new Player(1,"  x ");
 
     cout << "Welcome to FCAI X-O Game. :)\n";
     cout << "Press 1 if you want to play with computer: ";
     cin >> choice;
 
     if (choice != 1)
-        player[1] = new Player(2, "o");
+        player[1] = new Player(2,"  o ");
     else
         // Player pointer points to child
-        player[1] = new Random_Player("o", 3);  // Pass an integer parameter for Random_player
+        player[1] = new Random_Player("  o ", 3);  // Pass an integer parameter for Random_player
 
     X_O_Board *board = new X_O_Board;
     GameManager x_o_game(board, player);
