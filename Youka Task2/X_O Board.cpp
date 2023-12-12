6e931a737648007af8a0886997e9171c6330deca
YoussefEhab <114459225+Youkabob@users.noreply.github.com>
2023-12-12 18:51:01 +0200

    Update X_O Board.cpp

--- Youka Task2/X_O Board.cpp
+++ Youka Task2/X_O Board.cpp
@@ -21,7 +21,7 @@ X_O_Board::X_O_Board() {
             ss>>s2;
             s1+=s2;
             s1+=')';
-            boardArray[i][j] = s1;
+            boardArray[i][j] =s1;
         }
     }
 }//default constructor
@@ -29,14 +29,14 @@ X_O_Board::X_O_Board() {
 bool X_O_Board:: validcells(){
     for (int r = 0; r < ROWS; ++r) {
         for (int c = 0; c < COLUMNS; ++c) {
-            if (boardArray[r][c]!="x"||boardArray[r][c]!="o") return 1;
+            if (boardArray[r][c]!="  x "||boardArray[r][c]!="  o ") return 1;
         }
     }
     return 0;
 }
 
 bool X_O_Board::checkvalid(int x,int y) {
-    if(boardArray[x][y]=="x"||boardArray[x][y]=="o"){
+    if(boardArray[x][y]=="  x "||boardArray[x][y]=="  o "){
         return 0;
         }
     if(x==0&&y==2) {
@@ -72,17 +72,27 @@ bool X_O_Board::checkvalid(int x,int y) {
 
 void X_O_Board::update_board (int x, int y, string mark){
     boardArray[x][y]=mark;
+    n_moves++;
+}
+bool Board::is_draw(Player *player) {
+    if(n_moves==9&& !checkWin(player)){
+        cout<<"DRAW"<<endl;
+        return true;
+    }
+    else{
+        return false;
+    }
+
 }
 
 void X_O_Board::display()  {
-   cout<<"           "<<boardArray[0][2]<<endl;
-   cout<<"     "<<boardArray[1][1]<<" "<<boardArray[1][2]<<" "<<boardArray[1][3]<<" "<<endl;
+   cout<<"            "<<boardArray[0][2]<<endl;
+   cout<<"      "<<boardArray[1][1]<<" "<<boardArray[1][2]<<" "<<boardArray[1][3]<<" "<<endl;
    cout<<boardArray[2][0]<<" "<<boardArray[2][1]<<" "<<boardArray[2][2]<<" "<<boardArray[2][3]<<" "<<boardArray[2][4]<<endl;
 }
 
 bool X_O_Board::checkWin(Player* player)  {
     string symbol = player->get_symbol();
-
     //checking rows
     if(boardArray[1][1]==symbol&&boardArray[1][2]==symbol&&boardArray[1][3]==symbol){
         return true;
